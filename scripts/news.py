cc844c19baedaad2f01dd46fcef7b33073706823
Walley Yang <walleyyang@gmail.com>
2018-03-13 20:09:58 -0400

    Fixed news

--- scripts/news.py
+++ scripts/news.py
@@ -1,13 +1,5 @@
-#!/usr/bin/env python
-"""
-This file does the following:
-1. Retrieves the current trending news in the United States with the NewsAPI.
-2. Scrapes the article webpage with Beautiful Soup.
-3. Uses the Natural Language Processing library Spacy to extract entities.
-4. Uses GeoCoder to retrieve longitude and latitude coordinates from GeoNames.
-5. Adds the data to MongoDB.
-"""
 import json
+import re
 
 from datetime import datetime
 from bs4 import BeautifulSoup
@@ -21,19 +13,31 @@ import config
 def get_request(url):
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'}
     request = requests.get(url, headers=headers)
-
+    
     return request
-
+    
 def get_news():
     # Get top headines for the US
     url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + config.KEYS['NEWS_API']
+    
     request = get_request(url)
-
+    
     if request.status_code == 200:
         data = request.text
         articles = json.loads(data)
         create_json(articles)
 
+def article_exist(url_check, collection_name_check):
+    client = MongoClient()
+    db = client['news']
+    collection = db[collection_name_check]
+    url_count = collection.find({'url': url_check}).count()
+
+    if url_count > 0:
+        return True
+    
+    return False
+        
 def get_article(url):
     request = get_request(url)
     article = None
@@ -67,8 +71,14 @@ def get_article(url):
 
 def create_json(articles):
     nlp = spacy.load('en')
+    regex = re.compile('[^a-zA-Z]')
 
     for article in articles['articles']:
+        collection_name = regex.sub('', article['source']['name'])
+
+        if article_exist(article['url'], collection_name) is True:
+            continue
+
         # The GPE and LOC keys are used to retrieve latitude and longitudes from GeoNames.
         dictionary = {'insertDateTime':None, 'publishedDateTime':None, 'title':None, 'url':None,
                       'description':None, 'sourceName':None, 'coordinates':[], 'PERSON':[],
@@ -88,7 +98,7 @@ def create_json(articles):
         # We only want status code responses with 200
         if cleaned_article != None:
             nlp_article = nlp(cleaned_article)
-
+            
             for ent in nlp_article.ents:
                 if ent.label_ == 'GPE' or ent.label_ == 'LOC':
                     lat_long = geocoder.geonames(ent.text, key=config.KEYS['GEONAMES_USERNAME'])
@@ -97,8 +107,7 @@ def create_json(articles):
                         dictionary['coordinates'].append(lat_long.latlng)
                 else:
                     dictionary[ent.label_].append(ent.text)
-
-            collection_name = article['source']['name'].replace(" ", "")
+       
             dictionary_dump = json.dumps(dictionary)
             add_to_db(collection_name, json.loads(dictionary_dump))
 
@@ -107,5 +116,5 @@ def add_to_db(collection_name, data):
     db = client['news']
     collection = db[collection_name]
     collection.insert_one(data)
-
+      
 get_news()
